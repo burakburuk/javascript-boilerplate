285461a0bb4671a79a4ba7d3dea3322fa52bbd32
Burak Buruk <burakburuk@users.noreply.github.com>
2018-03-12 11:59:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,21 +3,48 @@ import MUtil from "./utils";
 var _host = 'http://my-json-server.typicode.com/burakburuk/javascript-test/';
 
 window.addEventListener('load', function () {
-
+    getAllPersonList();
     document.getElementById("btnTest").addEventListener("click", function (e) {
         testApi();
     });
 
 }, false);
 
+async function getAllPersonList() {
+    try {
+        let personList = await MUtil.ajax(_host + 'person');
+        personList = JSON.parse(personList);
+
+        let select = document.getElementsByTagName('select')[0];
+        personList.forEach(function (item) {
+            let option = document.createElement('option');
+            option.value = item.id;
+            option.textContent = item.name + " " + item.surname;
+            select.appendChild(option);
+        });
+
+    } catch (error) {
+        alert("Person list could not be fetched!");
+    }
+}
+
 async function testApi() {
     try {
-        let result = await MUtil.ajax(_host + 'person/1', {
-            method: 'GET'
-        });
-        console.log(result);
+        let resultBox = document.getElementsByTagName('textarea')[0];
+        let select = document.getElementsByTagName('select')[0];
+        if (select && select.value) {
+            let person = await MUtil.ajax(_host + 'person/' + select.value);
+            person = JSON.parse(person);
+
+            let address = await MUtil.ajax(_host + 'address/' + person.id);
+            address = JSON.parse(address);
+
+            resultBox.innerText = "Person : " + person.name + " " + person.surname + ", City : " + address.city;
+        } else {
+            resultBox.innerText = "Please Select Person!";
+        }
     } catch (err) {
-        console.log(err);
+        alert(err.statusText);
     }
 }
 
