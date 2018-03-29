deaf65b10fbf56921577012a1e735ab96976e2db
bburuk <burak.bburuk@gmail.com>
2018-03-29 19:05:02 +0300

    Template created for course 1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import '../conf/conf';
 import '../assets/main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/app";
 
 window.addEventListener('load', function () {
-    ReactDOM.render(<App/>, document.querySelector('#app'));
+    //ReactDOM.render(<App/>, document.querySelector('#app'));
 }, false);
 
 
