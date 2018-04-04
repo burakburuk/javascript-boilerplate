864ff0760ea1cf74d6b6fc44126e06b233d75e4a
bburuk <burak.bburuk@gmail.com>
2018-04-04 15:10:00 +0300

    Redux Counter example added!

--- src/components/app.js
+++ src/components/app.js
@@ -1,55 +1,19 @@
 import React from "react";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import Counter from './counter';
+import reducer from '../reducers/reducer';
+
+const store = createStore(reducer);
 
 class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0
-        };
-    }
-
-    componentWillUnmount = () => {
-        clearInterval(this.timer);
-    };
-
-    start = () => {
-        this.timer = setInterval(() => {
-            this.setState((prevState, props) => {
-                return {
-                    count: prevState.count + 1
-                }
-            })
-        }, 500);
-    };
-
-    stop = () => {
-        clearInterval(this.timer);
-    };
-
-    increment = () => {
-        this.setState((prevState, props) => {
-            return {
-                count: prevState.count + 1
-            }
-        })
-    };
-
-    decrement = () => {
-        this.setState((prevState, props) => {
-            return {
-                count: prevState.count - 1
-            }
-        })
-    };
-
     render() {
         return (
             <MuiThemeProvider>
-                <Counter count={this.state.count} increment={this.increment} decrement={this.decrement}
-                         start={this.start} stop={this.stop}></Counter>
+                <Provider store={store}>
+                    <Counter></Counter>
+                </Provider>
             </MuiThemeProvider>
         );
     }
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,33 +1,81 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import {connect} from 'react-redux';
 
-const Counter = props => {
-    return (
-        <div id="counter-div">
-            <h2>Counter</h2>
-            <table cellSpacing={2} cellPadding={5}>
-                <tbody>
-                <tr>
-                    <td>
-                        <RaisedButton onClick={props.decrement} label="-"/>
-                    </td>
-                    <td>
-                        <span id="counter-span">{props.count}</span>
-                    </td>
-                    <td>
-                        <RaisedButton onClick={props.increment} label="+"/>
-                    </td>
-                </tr>
-                <tr>
-                    <td colSpan={3}>
-                        <RaisedButton primary={true} onClick={props.start} label="Start"/>&nbsp;
-                        <RaisedButton secondary={true} onClick={props.stop} label="Stop"/>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    )
-};
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        //console.dir(props);
+        this.state = {
+            count: 0
+        };
+    }
 
-export default Counter;
+    componentWillUnmount = () => {
+        clearInterval(this.timer);
+    };
+
+    start = () => {
+        this.timer = setInterval(() => {
+            this.increment();
+        }, 500);
+    };
+
+    stop = () => {
+        clearInterval(this.timer);
+    };
+
+    increment = () => {
+        this.props.dispatch({type: 'INCREMENT'});
+    };
+
+    decrement = () => {
+        this.props.dispatch({type: 'DECREMENT'});
+    };
+
+    render() {
+        return (
+            <div id="counter-div">
+                <h2>Counter</h2>
+                <table cellSpacing={2} cellPadding={5}>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <RaisedButton onClick={this.decrement} label="-"/>
+                        </td>
+                        <td>
+                            <span id="counter-span">{this.props.count}</span>
+                        </td>
+                        <td>
+                            <RaisedButton onClick={this.increment} label="+"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={3}>
+                            <RaisedButton primary={true} onClick={this.start} label="Start"/>&nbsp;
+                            <RaisedButton secondary={true} onClick={this.stop} label="Stop"/>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+//“connect” the Counter component to Redux at the bottom:
+function mapStateToProps(state) {
+    return {
+        count: state.count
+    };
+}
+
+//Where previously we were exporting the component itself,
+//now we’re wrapping it with this connect function call.
+export default connect(mapStateToProps)(Counter);
+
+
+
+
+
+
