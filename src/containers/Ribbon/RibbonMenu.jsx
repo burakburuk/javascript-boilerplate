cf623e75af71a8f1e996e60b5203f194ea810c0d
Burak Buruk <burak.bburuk@gmail.com>
2018-05-20 23:29:02 +0300

    Accordion has been implemented

--- src/containers/Ribbon/RibbonMenu.jsx
+++ src/containers/Ribbon/RibbonMenu.jsx
@@ -5,7 +5,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import TabItemContainer from './TabItemContainer.jsx';
-import Typography from '@material-ui/core/Typography';
 import LayerTreePage from '../../components/pages/LayerTreePage.jsx';
 import UnitConverterPage from '../../components/pages/UnitConverterPage.jsx';
 
